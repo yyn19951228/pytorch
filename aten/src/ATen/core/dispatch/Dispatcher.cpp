84dd84eb9b53957a6b09439ebc9c8b81d550d465
cjshi <cjshi@devgpu175.prn2.facebook.com>
2020-06-11 06:44:34 -0700

    Make it an error to def() an operator multiple times
    Summary: Previously, if an operator is defined multiple times, it will be merged anyways. This code changes the behavior to spitting error if an operator is defined more than once.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    Make it an error to def() an operator multiple times
    
    Summary: Previously, if an operator is defined multiple times, it will be merged anyways. This code changes the behavior to spitting error if an operator is defined more than once.
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: ca1c89821c19e5d6a237cba760200044df997bbf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39459

--- aten/src/ATen/core/dispatch/Dispatcher.cpp
+++ aten/src/ATen/core/dispatch/Dispatcher.cpp
@@ -155,15 +155,9 @@ RegistrationHandleRAII Dispatcher::registerDef(FunctionSchema schema, std::strin
 void Dispatcher::checkSchemaCompatibility(const OperatorHandle& op, const FunctionSchema& schema, const std::string& debug) {
   TORCH_CHECK(op.schema() == schema, "Tried to register multiple operators with the same name and the same overload name but different schemas: ", schema, " (", debug, ") vs ", op.schema(), " (", op.debug(), ")");
   if (schema.isDefaultAliasAnalysisKind()) {
-    // [BACKWARDS COMPAT] If the *new* schema is the default alias analysis
-    // kind, for BC, we will accept it.  If we don't accept it, most extensions
-    // that override existing operators will stop working (as they generally did
-    // not specify alias information).
+    TORCH_CHECK(false, toString(op.operator_name()), " can not be defined more than once; function schema is: ", toString(schema));
   } else if (op.schema().isDefaultAliasAnalysisKind()) {
-    // [BACKWARDS COMPAT] If you POST-FACTO specify a non-default alias analysis
-    // kind after we already have a schema for a function, bong it in for BC
-    // reasons.
-    op.operatorIterator_->op.updateSchemaAliasAnalysis(schema.aliasAnalysis());
+    TORCH_CHECK(false, toString(op.operator_name()), " can not be defined more than once; function schema is: ", toString(schema));
   } else {
     TORCH_CHECK(op.schema().aliasAnalysis() == schema.aliasAnalysis(),
       "Tried to define the schema for ", toString(op.operator_name()), " with different alias analysis kinds: ",
